45b48cb810c83f130993796f3f0504f2fa2ae747
MihailMakarov2000 <125038197+MihailMakarov2000@users.noreply.github.com>
2023-03-02 00:35:29 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,66 @@
-﻿Console.WriteLine("Введите 3 числа:2,3,7");
-int num_1 = Convert.ToInt32(Console.ReadLine());
-int num_2 = Convert.ToInt32(Console.ReadLine());
-int num_3 = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Введите первое число: ");
+int number_A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number_B = Convert.ToInt32(Console.ReadLine());
+
+if  (number_A > number_B)
+{
+      Console.WriteLine("Первое число " + number_A + " больше чем второе " + number_B);
+}
+else
+{
+        Console.WriteLine("Второе число " + number_B + " больше чем первое " + number_A);
+}
+
+Console.WriteLine("Введите 3 числа:");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = Convert.ToInt32(Console.ReadLine());
+int number_3 = Convert.ToInt32(Console.ReadLine());
 
 int max = num_2;
 
-if (num_2 > max)
+if (number_2 > max)
 {
-    max = num_2;
+    max = number_2;
 }
 
-if (num_3 > max)
+if (number_3 > max)
 {
-    max = num_3;
+    max = number_3;
 }
 Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+
+Console.WriteLine("Введите число:");
+number = Convert.ToInt32(Console.ReadLine());
+
+if  (number % 2 == 1)
+{
+     Console.WriteLine("Число " + number + " является: нечетным");
+}
+else
+{
+        Console.WriteLine("Число " + number + "является: четным");
+}
+
+int i = 1;
+bool not = true;
+
+Console.WriteLine("Введите число:");
+number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + number);
+while  (i <= number)
+{
+            if  (i % 2 != 1)
+            {
+                  Console.Write(i + ", ");
+                  
+            }     
+              i++;
+}
+
+
+           {
+                Console.WriteLine("Нет чётных чисел!");
+           }
