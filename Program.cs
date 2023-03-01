f555180ba528c26d9c0634139299ca51a75ae0cd
MihailMakarov2000 <125038197+MihailMakarov2000@users.noreply.github.com>
2023-03-02 00:08:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("5");
+int namber_A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("7");
+int namber_B = Convert.ToInt32(Console.ReadLine());
+
+if (namber_A > namber_B)
+{
+    Console.WriteLine("Первое число" + namber_A + "больше чем второе" + namber_B);
+}
+else
+{
+    Console.WriteLine("Второе число" + namber_B + "больше чем второе" + namber_A);
+}
