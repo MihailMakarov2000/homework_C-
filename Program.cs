68ccbccd2a00705d467c0db0c556f93e6f055866
MihailMakarov2000 <125038197+MihailMakarov2000@users.noreply.github.com>
2023-03-05 18:00:41 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,66 +1,17 @@
-﻿Console.WriteLine("Введите первое число: ");
-int number_A = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите второе число: ");
-int number_B = Convert.ToInt32(Console.ReadLine());
-
-if  (number_A > number_B)
+﻿int Prompt(string message)
 {
-      Console.WriteLine("Первое число " + number_A + " больше чем второе " + number_B);
-}
-else
-{
-        Console.WriteLine("Второе число " + number_B + " больше чем первое " + number_A);
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
 }
 
-Console.WriteLine("Введите 3 числа:");
-int number_1 = Convert.ToInt32(Console.ReadLine());
-int number_2 = Convert.ToInt32(Console.ReadLine());
-int number_3 = Convert.ToInt32(Console.ReadLine());
-
-int max = num_2;
-
-if (number_2 > max)
+int namber = Prompt ("Введите трехзначное число > ");
+if (namber < 100 || namber >= 1000)
 {
-    max = number_2;
+    Console.WriteLine("Вы ввели не трехзначное число, пожалуста повторите ввод");
+    return;
 }
-
-if (number_3 > max)
-{
-    max = number_3;
-}
-Console.WriteLine("Наибольшее из введённых чисел -> " + max);
-
-Console.WriteLine("Введите число:");
-number = Convert.ToInt32(Console.ReadLine());
-
-if  (number % 2 == 1)
-{
-     Console.WriteLine("Число " + number + " является: нечетным");
-}
-else
-{
-        Console.WriteLine("Число " + number + "является: четным");
-}
-
-int i = 1;
-bool not = true;
-
-Console.WriteLine("Введите число:");
-number = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Чётные числа от 1 до " + number);
-while  (i <= number)
-{
-            if  (i % 2 != 1)
-            {
-                  Console.Write(i + ", ");
-                  
-            }     
-              i++;
-}
-
-
-           {
-                Console.WriteLine("Нет чётных чисел!");
-           }
+Console.WriteLine ($"Введённое число `{namber}`");
+int secondRank = namber / 10 % 10;
+Console.WriteLine ($"Вторая цифра `{secondRank}`");
